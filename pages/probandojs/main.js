caf9adf989208ce679965c40815a91733150418c
MarcelaMateus <mateusmarce02@gmail.com>
2024-07-19 14:27:49 -0300

    haciendo indice de masa corporal

---
+++ pages/probandojs/main.js
@@ -0,0 +1,6 @@
+let altura = prompt( "INgrese su altura");
+let peso= prompt ( "ingrese su peso actual");
+let icm=parseFloat(peso)/parseFloat(altura*altura);
+console.log("Su indice de masa corporal es"+ icm)
+alert ("su indice de masa corporal es" + icm);
+
